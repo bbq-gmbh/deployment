e45a4706305f1bd3feb1557b87dba28c317a95a4
c90x <e.0xcc3@gmail.com>
2025-11-07 21:57:50 +0100

    feat: Add local environment configuration and setup scripts for backend and frontend

---
+++ local/README.md
@@ -0,0 +1,2 @@
+# Lokales Setup
+
--- local/setup.sh
+++ local/setup.sh
@@ -2,3 +2,35 @@ mkdir git
 
 git clone https://github.com/bbq-gmbh/backend ./git/backend
 git clone https://github.com/bbq-gmbh/frontend2 ./git/frontend
+
+
+current_dir=$(pwd)
+
+
+# check if uv exists
+command -v uv >/dev/null 2>&1 || { echo "uv is not installed"; exit 1; }
+
+cd ./git/backend
+
+# copy environment files
+cp "$current_dir/.backend.env" .env
+
+uv sync --frozen
+
+cd "$current_dir"
+
+
+# check if npm exists
+command -v npm >/dev/null 2>&1 || { echo "npm is not installed"; exit 1; }
+
+cd ./git/frontend
+
+# copy environment files
+cp "$current_dir/.frontend.env" .env
+
+npm install
+
+cd "$current_dir"
+
+
+echo "done!"
